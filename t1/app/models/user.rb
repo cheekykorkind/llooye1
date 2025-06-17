6403df755d5589945adc0407a710eb11091cc601
cheekykorkind <cheekorkind@gmail.com>
2025-06-17 08:33:48 -0400

    user까지통과

--- t1/app/models/user.rb
+++ t1/app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  has_secure_password
+
+  validates :nickname, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
